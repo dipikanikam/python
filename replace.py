5784eacd43d7b1865b6b253c93f55d09022de8ee
dipikanikam <91178612+dipikanikam@users.noreply.github.com>
2021-11-22 20:49:22 +0530

    oop cocept example

---
+++ replace.py
@@ -0,0 +1,1 @@
+def remove_char(s1,s2):    print(s1.replace(s2, ''))s1 = input("please give a String : ")s2 = input("please give a Character to remove : ")remove_char(s1,s2)
---
+++ tringle.py
@@ -0,0 +1,1 @@
+n=int(input("enter"))for i in range(n):    print((' ' * (n - i - 1)) + ('* ') * (i + 1))for i in range(n-1):    print(' ' * i + '* ' * (n - i))print( )
